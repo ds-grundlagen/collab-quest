fdff3feade4b05239a9df56db788988469caa5ff
chenxuanye29-ops <chenxuanye29@gmail.com>
2025-09-09 14:49:53 +0000

    Tue, Sep 9, 2025, 4:49 PM +02:00

--- scripts/iwant.py
+++ scripts/iwant.py
@@ -2,10 +2,10 @@
 
 #i create a selection to see which city I would go
 T = int(input("how many degree of that city"))
-
+time = float(input("how many hours for getting there"))
 if T > 23 :
     print("the temperature looks fine,but how long for getting there?")
-    time = float(input("how many hours for getting there"))
+   
 elif time > 12:
     print("too long to go")
 else:
