b9560073ada248a03cb6556a10102c2f43bdd770
chenxuanye29-ops <chenxuanye29@gmail.com>
2025-09-09 14:33:54 +0000

    Tue, Sep 9, 2025, 4:33 PM +02:00

---
+++ docs/iwant.md
@@ -0,0 +1,2 @@
+I want to know I should visit it or not,and I care about the temperature and distance so much.
+so if the temperature and distance are both fine for me,then it is done.
---
+++ scripts/iwant.py
@@ -0,0 +1,13 @@
+#I want to go travling to france,and I want to choose a city to go
+
+#i create a selection to see which city I would go
+T = int(input("how many degree of that city"))
+
+if T > 23 :
+    print("the temperature looks fine,but how long for getting there?")
+    time = float(input("how many hours for getting there"))
+elif time > 12:
+    print("too long to go")
+else:
+
+    print("yes plesase go there")
