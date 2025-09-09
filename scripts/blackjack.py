8f8cef194cec3e52200a2cf0d9a7bd53786f6778
Jarred Green <astrojarred@gmail.com>
2025-09-09 16:37:39 +0200

    Removed card 44

--- scripts/blackjack.py
+++ scripts/blackjack.py
@@ -5,7 +5,7 @@ This code plays a came of blackjack with the user
 from random import shuffle
 
 
-RANKS = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A", "44"]
+RANKS = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 SUITS = ["♠", "♥", "♦", "♣"]
 
 
