0f6faf760be31a097de356e90131f8937a1e50e8
gallevi123 <gal.levi2@gmail.com>
2025-09-09 13:36:24 +0000

    Tue, Sep 9, 2025, 3:36 PM +02:00

--- dates.sh
+++ dates.sh
@@ -1,3 +1,6 @@
 echo "how are you today?
 got any plans for date"
 
+#the code should ask you "how are you today?"
+#in a new line you're being asked "got any plans for (date)?"
+#(Date) should be the date function
